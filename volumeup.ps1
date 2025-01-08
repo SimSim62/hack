3844b2845e350aac5cd84652fc595c5ce43f4a1b
SimSim62 <barbrysimon1@gmail.com>
2025-01-08 21:51:18 +0100

    Add files via upload

--- volumeup.ps1
+++ volumeup.ps1
@@ -1,1 +1,7 @@
+# Obtenir une collection de tous les périphériques de sortie audio
+$outputs = Get-AudioDevice -List * -Role output
 
+# Boucler sur chaque périphérique et régler le volume au maximum
+foreach ($output in $outputs) {
+    Set-AudioDevice -PlaybackDevice $output.Name -SetVolume -Volume 100
+}
