75a2d894de6cb7466afc398d8ad91a5deefc34f0
SimSim62 <barbrysimon1@gmail.com>
2025-01-08 22:39:00 +0100

    Delete volumeup.ps1

--- volumeup.ps1
+++
@@ -1,7 +0,0 @@
-# Obtenir une collection de tous les périphériques de sortie audio
-$outputs = Get-AudioDevice -List * -Role output
-
-# Boucler sur chaque périphérique et régler le volume au maximum
-foreach ($output in $outputs) {
-    Set-AudioDevice -PlaybackDevice $output.Name -SetVolume -Volume 100
-}
